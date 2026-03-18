1f8963a67d1e609fbc30e1d1963ad47ae5200093
belladaq <itd2007@nyu.edu>
2026-03-18 12:32:08 -0400

    Initial image upload

--- front-end/src/components/CreateNew.jsx
+++ front-end/src/components/CreateNew.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateNew = ({ onImageSelect }) => {
+  const [imageFile, setImageFile] = useState(null)
+  const [previewUrl, setPreviewUrl] = useState(null)
+
   const handleImageChange = (event) => {
     const file = event.target.files?.[0]
     if (!file) return
+
+    setImageFile(file);
+
+    const url = URL.createObjectURL(file);
+    setPreviewUrl(url)
 
     console.log('Selected file in CreateNew:', file)
     if (onImageSelect) {
@@ -26,6 +34,14 @@ const CreateNew = ({ onImageSelect }) => {
         style={{ display: 'none' }}
         onChange={handleImageChange}
       />
+
+      {previewUrl && (
+        <div className='preview-box'>
+          <img src={previewUrl} alt='Upload preview'></img>
+        </div>
+      )
+      
+      }
     </div>
   )
 }
