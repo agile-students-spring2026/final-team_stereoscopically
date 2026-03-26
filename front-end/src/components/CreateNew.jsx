16700b003ad45cc9be07bf6d9500baef7c4fb3e3
fwinford <fwinford12@gmail.com>
2026-03-26 18:31:51 +0100

    added back route for api

--- front-end/src/components/CreateNew.jsx
+++ front-end/src/components/CreateNew.jsx
@@ -1,46 +1,28 @@
 const CreateNew = ({ onImageSelect, onVideoSelect, isLoading = false, errorMessage = null }) => {
   const statusMessage = errorMessage || (isLoading ? 'Loading media from Pixabay…' : null)
 
-  const handleImageChange = (event) => {
-    const file = event.target.files?.[0]
-    if (!file) return
-    onImageSelect?.(file)
-  }
-
-  const handleVideoChange = (event) => {
-    const file = event.target.files?.[0]
-    if (!file) return
-    onVideoSelect?.(file)
-  }
-
   return (
     <div className="card create-new">
       <h2>Create New</h2>
 
       <div className="upload-options">
-        <label htmlFor="image-upload" className="upload-button">
+        <button
+          type="button"
+          className="upload-button"
+          onClick={() => onImageSelect?.()}
+          disabled={isLoading}
+        >
           Upload Image
-        </label>
+        </button>
 
-        <input
-          id="image-upload"
-          type="file"
-          accept="image/*"
-          className="hidden-file-input"
-          onChange={handleImageChange}
-        />
-
-        <label htmlFor="video-upload" className="upload-button">
+        <button
+          type="button"
+          className="upload-button"
+          onClick={() => onVideoSelect?.()}
+          disabled={isLoading}
+        >
           Upload Video
-        </label>
-
-        <input
-          id="video-upload"
-          type="file"
-          accept="video/*"
-          className="hidden-file-input"
-          onChange={handleVideoChange}
-        />
+        </button>
       </div>
 
       {statusMessage && (
